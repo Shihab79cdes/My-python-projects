da77face180d4c1db77c7bf16db95be54f16da45
Shihab79cdes <shihabmallick02@gmail.com>
2025-09-30 21:56:50 +0530

    Update simple_project.py

--- day1/simple_project.py
+++ day1/simple_project.py
@@ -1,1 +1,14 @@
+one=input("what is your name?")
+print("ok "+ one + " !!")
 
+two=input("how are you!!")
+print("Nice to hear that" + " !!")
+
+three=input("So tell me your qualification: ")
+print("Awesome")
+
+four=input("so what is goal by doing coding: ")
+print("Good luck for your journey")
+
+five=input("how much coding knowledge you have beginner"+" " "intermediate"+ " " "or" + " " "experience"+ "?")
+print("GOOD keep progress"+"!!")
